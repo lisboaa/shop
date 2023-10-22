a2bd7c53b8c406ae00e548647b0b174bf2408c98
Douglas Lisboa <douglasfernandolisboaa@gmail.com>
2023-10-22 09:56:14 -0300

    feat: Add category component and types icons

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,18 +1,24 @@
 "use client"
 
 import Image  from "next/image"
+import Categories from "./components/categories";
 
 export default function Home() {
-    return (  
-        <div className="p-5">
-            <Image 
-                src="/banner-home.png"
-                height={0}
-                width={0}
-                className="h-auto- w-full"
-                sizes="100vw"
-                alt="Até 55% de desconto so esse mês!"
-                />
-        </div>
+    return (
+        <>
+            <div className="p-5">
+                <Image 
+                    src="/banner-home.png"
+                    height={0}
+                    width={0}
+                    className="h-auto- w-full"
+                    sizes="100vw"
+                    alt="Até 55% de desconto so esse mês!"
+                    />
+                <div className="mt-8">
+                    <Categories/>
+                </div>
+            </div>
+        </>
     );
 }
